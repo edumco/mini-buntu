28455119047d6a7c0736c4d0765397645f38b7db
José Eduardo Montenegro Cavalcanti de Oliveira <jose.eduardo.mco@gmail.com>
2021-04-12 00:44:07 -0300

    Update openjdk-11-jdk.sh

--- openjdk-11-jdk.sh
+++ openjdk-11-jdk.sh
@@ -1,5 +1,10 @@
-JAVA
-sudo apt-get install openjdk-11-jdk
+#!/bin/sh
+
+# Installs OpenJDK 11
+sudo apt install --assume-yes openjdk-11-jdk
+
+# Creates JAVA_HOME on the user profile
 echo "export JAVA_HOME=/usr/bin/" > ~/.profile
 
-
+# Load the profile
+source ~/.profile
